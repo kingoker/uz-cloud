3e1203add2ce95cf9999d4003d89a3d45469eea0
Akbar <zolotojznak@gmail.com>
2023-06-12 12:34:11 +0500

    admin panel updated for phone numbers

--- root/phoneNumbers/admin.py
+++ root/phoneNumbers/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from .models import *
 
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'internal_number', 'phone_number')
+    search_fields = ('full_name',)
+    list_filter = ('position', 'birth_date')
+
 
 admin.site.register(Department)
 admin.site.register(Position)
-admin.site.register(CustomUser)
+admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(Structure)
